b457455394342227e9e8705bb7e0c698461904ed
Sergey Karchevsky <sergey.ext@gmail.com>
2016-11-04 17:25:13 +0700

    Fixed texture parameter bug.
    Two lines of buggy code tuned parameters for never-used 2D textures.

--- viewport.cpp
+++ viewport.cpp
@@ -81,8 +81,8 @@ void Viewport::initializeGL() {
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_BASE_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAX_LEVEL, 0);
     glTexImage1D(GL_TEXTURE_1D, 0, GL_R16F, randTexSize, 0, GL_RED, GL_FLOAT, randTexData);
     glUniform1i(glGetUniformLocation(grassShader.programId(), "urandom01"), 0);
     // Queries.
